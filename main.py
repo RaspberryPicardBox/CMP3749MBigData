6b31c661e20c9d82519ae5b5a6b279a122e31232
Raspi <raspberrypicardbox@gmail.com>
2021-12-07 14:53:21 +0000

    Add repairData function

--- main.py
+++ main.py
@@ -13,8 +13,18 @@ import matplotlib as plt
 import numpy as np
 
 
+def repairData(df):
+    print("Cleaning df of count: {0}".format(df.count()))
+    df = df.dropDuplicates()
+    df = df.dropna()
+    print("Done cleaning df - new count: {0}".format(df.count()))
+    return df
+
+
 if __name__ == '__main__':
     spark = SparkSession.builder.getOrCreate()
 
     df = spark.read.csv("nuclear_plants_small_dataset.csv", inferSchema=True, header=True)
-    df.show()
+    df = repairData(df)
+
+
