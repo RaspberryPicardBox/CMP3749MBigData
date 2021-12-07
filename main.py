ef0143a0120c4803a901f6b1d5c673f4c5a33705
Raspi <raspberrypicardbox@gmail.com>
2021-12-07 14:35:57 +0000

    Find Spark and load dataset

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+def openSparkDirectory():
+    direc = open("directories.txt", "r")
+    ret = direc.read()
+    direc.close()
+    return ret
+
+
+import findspark
+findspark.init(openSparkDirectory())
 import pyspark
 from pyspark.sql import SparkSession
 import matplotlib as plt
 import numpy as np
 
+
 if __name__ == '__main__':
-    print("Hello world!")
+    spark = SparkSession.builder.getOrCreate()
+
+    df = spark.read.csv("nuclear_plants_small_dataset.csv", inferSchema=True, header=True)
+    df.show()
