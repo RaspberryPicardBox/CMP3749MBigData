6d1f3697153b6dac35125b88a20c84b9ba56075b
Raspi <raspberrypicardbox@gmail.com>
2022-01-10 18:30:06 +0000

    Added correlation matrix

--- main.py
+++ main.py
@@ -11,9 +11,11 @@ findspark.init(openSparkDirectory())
 import pyspark
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as pf
+from pyspark.ml.stat import Correlation
+from pyspark.ml.feature import VectorAssembler
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sn
+import seaborn as sns
 
 
 def repairData(df):  # Repair the data by removing duplicates and Null values
@@ -50,11 +52,18 @@ def summary(df):  # Summary of the data
         plt.show()
 
 
+def correlation(df): # Return correlation matrix of df
+    df = df.drop("Status")
+    df = df.toPandas()
+    sns.heatmap(df.corr(), vmin=-1, vmax=1, annot=True)
+    plt.show()
+
+
 if __name__ == '__main__':
     spark = SparkSession.builder.getOrCreate()
 
     df = spark.read.csv("nuclear_plants_small_dataset.csv", inferSchema=True, header=True)  # Load data from csv file
-    #df = repairData(df)  # Repair the data
+    df = repairData(df)  # Repair the data
 
     print("Summary where Status is Normal")
     summary(df.where(df["Status"] == "Normal"))  # Summarise the data where Status is Normal
@@ -62,5 +71,7 @@ if __name__ == '__main__':
     print("Summary where Status is Abnormal")
     summary(df.where(df["Status"] == "Abnormal"))  # Summarise the data where Status is Abnormal
 
-    # TODO: Show correlation matrix for each feature
+    print("Correlation matrix of DF")
+    correlation(df)  # Shows correlation matrix of df
+
     # TODO: Decision tree
