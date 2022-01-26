2bba7e069fcaf978fd648a649ac66f80bd711d71
Raspi <raspberrypicardbox@gmail.com>
2022-01-26 11:06:41 +0000

    Completed PySpark ML decision tree and lsvm

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ def openSparkDirectory():  # Finding PySpark on local machine
 
 
 import findspark
+
 findspark.init(openSparkDirectory())
 
 import pyspark
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as pf
+from pyspark.sql.functions import lit, when, regexp_replace
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import DecisionTreeClassifier, LinearSVC
+from pyspark.ml.feature import StringIndexer, VectorIndexer, VectorAssembler
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
-import sklearn
-import sklearn.model_selection
-from sklearn import tree
-from sklearn.tree import DecisionTreeClassifier, plot_tree
 
 
 def repairData(df):  # Repair and return the data by removing duplicates and Null values
@@ -54,7 +55,7 @@ def summary(df):  # Show summary of the data
         plt.show()
 
 
-def correlation(df): # Show correlation matrix of df
+def correlation(df):  # Show correlation matrix of df
     df = df.drop("Status")
     df = df.toPandas()
     sns.heatmap(df.corr(), vmin=-1, vmax=1, annot=True)
@@ -62,27 +63,44 @@ def correlation(df): # Show correlation matrix of df
 
 
 def split(df):  # Return split data for train and test
-    train, test = df.randomSplit([0.7, 0.3])
+    vectorAssembler = VectorAssembler(inputCols=df.drop('Status').columns, outputCol='features')
+    df = vectorAssembler.transform(df).drop('Power_range_sensor_1', 'Power_range_sensor_2', 'Power_range_sensor_3',
+                                            'Power_range_sensor_4', 'Pressure_sensor_1', 'Pressure_sensor_2',
+                                            'Pressure_sensor_3', 'Pressure_sensor_4', 'Vibration_sensor_1',
+                                            'Vibration_sensor_2', 'Vibration_sensor_3', 'Vibration_sensor_4')
+
+    stringIndexer = StringIndexer(inputCol="Status", outputCol="labelIndex")
+    df = stringIndexer.fit(df).transform(df).drop('Status')
+
+    (train, test) = df.randomSplit([0.7, 0.3])
 
     return train, test
 
 
 def decTree(train, test):  # Show decision tree
-    plt.figure()
-    X = train.drop("Status").toPandas()
-    Y = train.select("Status").toPandas()
-    decision_tree = DecisionTreeClassifier()
-    decision_tree = decision_tree.fit(X, Y)
-    tree.plot_tree(decision_tree)
-    plt.savefig("Train.png")
-    plt.show()
-    plt.close()
-    plt.figure()
-    decision_tree.predict(test.drop("Status").toPandas())
-    tree.plot_tree(decision_tree)
-    plt.savefig("Test.png")
-    plt.show()
-    plt.close()
+    dt = DecisionTreeClassifier(labelCol='labelIndex', featuresCol='features')
+    model = dt.fit(train)
+    predictions = model.transform(test)
+    predictions.show()
+
+    evaluator = MulticlassClassificationEvaluator(labelCol="labelIndex", predictionCol="prediction", metricName="accuracy")
+    accuracy = evaluator.evaluate(predictions)
+
+    print("Test error = {}".format(1.0-accuracy))
+    print("Accuracy = {}".format(accuracy))
+
+
+def linearSupportVector(train, test):
+    lsvc = LinearSVC(maxIter=50, regParam=0.0, labelCol='labelIndex', featuresCol='features')
+    model = lsvc.fit(train)
+    predictions = model.transform(test)
+    predictions.show()
+
+    evaluator = MulticlassClassificationEvaluator(labelCol="labelIndex", predictionCol="prediction", metricName="accuracy")
+    accuracy = evaluator.evaluate(predictions)
+
+    print("Test error = {}".format(1.0 - accuracy))
+    print("Accuracy = {}".format(accuracy))
 
 
 if __name__ == '__main__':
@@ -90,21 +108,22 @@ if __name__ == '__main__':
 
     df = spark.read.csv("nuclear_plants_small_dataset.csv", inferSchema=True, header=True)  # Load data from csv file
     df = repairData(df)  # Repair the data
-    """
-    print("-------Summary where Status is Normal-------\n")
+
+    """print("-------Summary where Status is Normal-------\n")
     summary(df.where(df["Status"] == "Normal"))  # Summarise the data where Status is Normal
 
     print("-------Summary where Status is Abnormal-------\n")
     summary(df.where(df["Status"] == "Abnormal"))  # Summarise the data where Status is Abnormal
 
     print("-------Correlation matrix of DF-------\n")
-    correlation(df)  # Shows correlation matrix of df
-    """
+    correlation(df)  # Shows correlation matrix of df"""
+
     print("-------Shuffling and splitting data...-------\n")
     train, test = split(df)
-    print("-------Train Set-------\nLength: {}\n".format(train.count()))
+    """print("-------Train Set-------\nLength: {}\n".format(train.count()))
     train.show()
     print("-------Test Set-------\nLength: {}\n".format(test.count()))
-    test.show()
+    test.show()"""
 
-    decTree(train, test)
+    # decTree(train, test)
+    linearSupportVector(train, test)
